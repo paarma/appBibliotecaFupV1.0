5845b902a6bf8591e0d23d6fab4cc447e3e3a6af
Pablo <pablo@linux-vnf4.site>
2015-03-15 17:46:47 -0500

    Proyecto base

---
+++ app/src/main/java/com/developer/pablo/appbibliotecafup/Activity_login.java
@@ -0,0 +1,57 @@
+package com.developer.pablo.appbibliotecafup;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.developer.pablo.appbibliotecafup.util.TextChangeListener;
+
+
+public class Activity_login extends ActionBarActivity {
+
+    private EditText tbxUsuario, tbxClave;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        inicializarComponentes();
+    }
+
+    private void inicializarComponentes() {
+        tbxUsuario = (EditText) findViewById(R.id.tbxUsuario);
+        tbxClave = (EditText) findViewById(R.id.tbxClave);
+
+        //Funcionalidad que habilita el boton Aceptar cuando se ingrese texto
+        //en el campo Usuario. Se sobreescribe el metodo onTextChanged con la clase creada TextChangeListener
+        tbxUsuario.addTextChangedListener(new TextChangeListener(){
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                btnLogin = (Button) findViewById(R.id.btnLogin);
+                btnLogin.setEnabled(!s.toString().trim().isEmpty());
+            }
+        });
+    }
+
+    public void login(View view){
+        String msn = String.format(" Usuario ingresado: %s",tbxUsuario.getText());
+        //Mensaje visual
+        Toast.makeText(this, msn, Toast.LENGTH_SHORT).show();
+        btnLogin.setEnabled(false);
+        limpiar();
+    }
+
+    private void limpiar() {
+        tbxUsuario.getText().clear();
+        tbxClave.getText().clear();
+
+        //Regresa el foco al campo Usuario
+        tbxUsuario.requestFocus();
+    }
+
+}
---
+++ app/src/main/java/com/developer/pablo/appbibliotecafup/util/TextChangeListener.java
@@ -0,0 +1,25 @@
+package com.developer.pablo.appbibliotecafup.util;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+/**
+ * Created by pablo on 15/03/15.
+ * Adaptador para implementar los metodos de una interfaz (ChangeListener)
+ */
+public class TextChangeListener implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
