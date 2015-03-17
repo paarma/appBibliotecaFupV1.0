1f59fe6497a59fb835cb038548e4e28cc2260548
pablo <paarma>
2015-03-17 17:14:31 -0500

    Capturar objeto al seleccionar un elemento del ArrayAdapter

--- app/src/main/java/com/developer/pablo/appbibliotecafup/Activity_listado_libros.java
+++ app/src/main/java/com/developer/pablo/appbibliotecafup/Activity_listado_libros.java
@@ -2,8 +2,11 @@ package com.developer.pablo.appbibliotecafup;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.developer.pablo.appbibliotecafup.modelo.Libro;
 import com.developer.pablo.appbibliotecafup.util.LibroListAdapter;
@@ -19,6 +22,8 @@ public class Activity_listado_libros extends Activity {
     //private List<Libro> listaLibros;
     private ArrayAdapter<Libro> adapterLibro;
     private ListView libroListView;
+
+    private Libro libroSeleccionado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +43,7 @@ public class Activity_listado_libros extends Activity {
         ///////////// crear tarea listado libros
         //adapterLibro = new LibroListAdapter(this, new ArrayList<Libro>());
 
-        List<Libro> listaLibro = new ArrayList<Libro>();
+        final List<Libro> listaLibro = new ArrayList<Libro>();
 
         Libro lib1 = new Libro();
         lib1.setTitulo("Libro 1");
@@ -54,6 +59,16 @@ public class Activity_listado_libros extends Activity {
 
         adapterLibro = new LibroListAdapter(this, listaLibro);
         libroListView.setAdapter(adapterLibro);
+
+        //Evento al seleccionar un elemento de la lista
+        libroListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> padre, View vista, int posicion, long id) {
+                libroSeleccionado = listaLibro.get(posicion);
+                String msn = "Seleccionado :"+libroSeleccionado.getTitulo();
+                Toast.makeText(Activity_listado_libros.this, msn, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
