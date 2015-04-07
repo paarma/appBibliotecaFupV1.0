a72506d4503e3d2bf90772eee8205b59bc9edde1
pablo <paarma>
2015-04-07 09:53:10 -0500

    Se agrega menu contextual para las opciones del admin

--- app/src/main/java/com/developer/pablo/appbibliotecafup/Activity_admin.java
+++ app/src/main/java/com/developer/pablo/appbibliotecafup/Activity_admin.java
@@ -3,6 +3,9 @@ package com.developer.pablo.appbibliotecafup;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,21 +14,98 @@ import android.widget.Button;
  */
 public class Activity_admin extends Activity {
 
-    Button btnCrearLibro;
+    Button btnLibro, btnAutor;
+    private int opcionMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        btnLibro = (Button) findViewById(R.id.btnLibro);
+        btnAutor = (Button) findViewById(R.id.btnAutor);
+
+        //Se genera menu contextual
+        registerForContextMenu(btnLibro);
+        registerForContextMenu(btnAutor);
     }
 
-    public void crearLibro(View view){
-        Intent goCrearLibro = new Intent(Activity_admin.this, Activity_crear_libro.class);
-        startActivity(goCrearLibro);
+    /**
+     * Metodos para menu contextual
+     * @param menu
+     * @param v Componente visual el cual llama al menu contextual
+     * @param menuInfo
+     */
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        //Se agrega titulo al menu
+        menu.setHeaderTitle("Opciones");
+
+        //Se identifica que boton genera el llamado al menu contextual
+        switch (v.getId()){
+            case R.id.btnLibro:
+                opcionMenu = 1;
+                menu.add(0, v.getId(), 0, "Listado Libros");
+                menu.add(1, v.getId(), 1, "Crear Libro");
+                break;
+            case R.id.btnAutor:
+                opcionMenu = 2;
+                menu.add(2, v.getId(), 0, "opcion 3");
+                menu.add(3, v.getId(), 1, "opcion 4");
+                break;
+        }
     }
 
-    public void listadoLibros(View view){
-        Intent goListadoLibros = new Intent(Activity_admin.this, Activity_listado_libros.class);
-        startActivity(goListadoLibros);
+    /**
+     * Metodo para especificar las acciones del menu seleccionado
+     * @param item Item seleccionado del menu
+     * @return
+     */
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        //return super.onContextItemSelected(item);
+
+        try {
+
+            //Se especifican las acciones para cada boton
+            switch (opcionMenu){
+                case 1:
+                    //Se especifican las acciones de cada item
+
+                    if(item.getGroupId() == 0){
+                        Log.i("Acticity_admin.java", "*************> menu libros >>>> opcion: "+item.getTitle());
+                        Intent goListadoLibros = new Intent(Activity_admin.this, Activity_listado_libros.class);
+                        startActivity(goListadoLibros);
+                    }
+
+                    if(item.getGroupId() == 1){
+                        Log.i("Acticity_admin.java", "*************> menu libros >>>> opcion: "+item.getTitle());
+                        Intent goCrearLibro = new Intent(Activity_admin.this, Activity_crear_libro.class);
+                        startActivity(goCrearLibro);
+                    }
+                    break;
+
+                case 2:
+                    //Se especifican las acciones de cada item
+
+                    if(item.getGroupId() == 2){
+                        Log.i("Acticity_admin.java", "*************> menu Autores >>>> opcion: "+item.getTitle());
+                    }
+
+                    if(item.getGroupId() == 3){
+                        Log.i("Acticity_admin.java", "*************> menu Autores >>>> opcion: "+item.getTitle());
+                    }
+                    break;
+
+            }
+
+        }catch (Exception e){
+            return false;
+        }
+
+        return true;
     }
+
 }
